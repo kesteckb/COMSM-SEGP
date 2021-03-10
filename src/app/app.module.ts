4c59eaa21fe3a3ab8f66bc2163bdc6ff93c826ba
Zack Turman <zkturman@gmail.com>
2021-03-10 16:35:51 +0000

    there was another app.module.ts change... haha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
-<<<<<<< HEAD
-import { GameOverComponent } from './game-over/game-over.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> 562ddaa3e851c9e85ee377ca748dfaa128607932
+import { GameOverComponent } from './game-over/game-over.component';
 
 @NgModule({
   declarations: [
