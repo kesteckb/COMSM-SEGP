9d94db19282465c35cb5c0f35babf0177ea93d3a
Zack Turman <zkturman@gmail.com>
2021-03-10 10:20:58 +0000

    added component files for a game over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
+import { GameOverComponent } from './game-over/game-over.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    GameOverComponent
   ],
   imports: [
     BrowserModule,
