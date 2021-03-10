ad5abe17d27347611223459dbb335df9718ed272
Zack Turman <zkturman@gmail.com>
2021-03-10 16:26:33 +0000

    added gameover component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameOverComponent } from './game-over/game-over.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: 'game-over', component: GameOverComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
