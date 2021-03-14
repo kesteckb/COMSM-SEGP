96b82d250ed54ddcc7c621e0e6e96c9a7f2a3231
Zack Turman <zkturman@gmail.com>
2021-03-14 18:31:42 +0000

    adding unclamined components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { HomeComponent } from './home/home.component';
 import { GameOverComponent } from './game-over/game-over.component';
+import { GameWinComponent } from './game-win/game-win.component';
+import { FoodQuizComponent } from './food-quiz/food-quiz.component';
+import { GenericErrorComponent } from './generic-error/generic-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     HomeComponent,
-    GameOverComponent
+    GameOverComponent,
+    GameWinComponent,
+    FoodQuizComponent,
+    GenericErrorComponent
   ],
   imports: [
     BrowserModule,
