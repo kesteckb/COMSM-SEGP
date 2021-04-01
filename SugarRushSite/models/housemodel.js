702b5e758267b46c523a466b4c6ba306a638a669
Zack Turman <zkturman@gmail.com>
2021-04-01 14:25:48 +0100

    initial hobbit and house schema

---
+++ SugarRushSite/models/housemodel.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var houseSchema = Schema({
+   name : {type: String, required: yes },
+   resident : {type: Scheme.Type.ObjectId, ref: 'Hobbit', required: true }
+});
+
+var House = mongoose.model('House', houseSchema);
