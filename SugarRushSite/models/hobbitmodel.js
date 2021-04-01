ca921f645f75f6d5711bc14802adba7725b501b7
Zack Turman <zkturman@gmail.com>
2021-04-01 14:32:50 +0100

    removed spaces before colons

--- SugarRushSite/models/hobbitmodel.js
+++ SugarRushSite/models/hobbitmodel.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var hobbitSchema = Schema({
-   name : {type: String, required: true },
-   sugarTolerance : { type: Number, min: 0, required: true },
-   house : { type: Schema.Types.ObjectId, ref: 'House', required: true },
-   preferredFood : [{type: Schema.Type.ObjectId, ref: 'FoodItem' }]
+   name: {type: String, required: true },
+   sugarTolerance: { type: Number, min: 0, required: true },
+   house: { type: Schema.Types.ObjectId, ref: 'House', required: true },
+   preferredFood: [{type: Schema.Type.ObjectId, ref: 'FoodItem' }]
 });
 
 var Hobbit = mongoose.model('Hobbit', hobbitSchema);
