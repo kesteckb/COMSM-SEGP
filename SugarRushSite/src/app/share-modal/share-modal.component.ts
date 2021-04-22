1cd91015dbd19eeb141a9ac1ceff790654909acb
Zack Turman <zkturman@gmail.com>
2021-04-22 10:00:58 +0100

    adding share links to gameened

--- SugarRushSite/src/app/share-modal/share-modal.component.ts
+++ SugarRushSite/src/app/share-modal/share-modal.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NgSocialLinksService } from 'ng-social-links';
 
 @Component({
-  selector: 'app-share-modal',
-  templateUrl: './share-modal.component.html',
-  styleUrls: ['./share-modal.component.css']
+   selector: 'app-share-modal',
+   templateUrl: './share-modal.component.html',
+   styleUrls: ['./share-modal.component.css']
 })
 export class ShareModalComponent implements OnInit {
+   facebookShareLink: String;
+   twitterShareLink: String;
+   redditShareLink: String;
 
-  constructor() { }
+   constructor(private socialLinks: NgSocialLinksService) { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+      this.facebookShareLink = this.socialLinks.getShareLink('fb');
+      this.twitterShareLink = this.socialLinks.getShareLink('tw');
+      this.redditShareLink = this.socialLinks.getShareLink('re');
+
+   }
 
 }
