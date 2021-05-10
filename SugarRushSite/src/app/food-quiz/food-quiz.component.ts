fdd8ac014fb13729af01645de3b252025d73e904
Zack Turman <zkturman@gmail.com>
2021-05-10 12:24:29 +0100

    static filling progress bar

--- SugarRushSite/src/app/food-quiz/food-quiz.component.ts
+++ SugarRushSite/src/app/food-quiz/food-quiz.component.ts
@@ -27,12 +27,6 @@ export class FoodQuizComponent implements OnInit {
    public answers: any = [];
    currentMeal: number = 0;
 
-  // public foodItems = [
-  //   {name: 'Banana', sugarAmount: 12.0},
-  //   {name: 'Apple', sugarAmount: 10.0},
-  //   {name: 'Protein Shake', sugarAmount: 23.0},
-  // ];
-
    public meals = [
       {name: MealTitle.BREAKFAST, foodChoices: [0, 1, 2]},
       {name: MealTitle.SECOND_BREAKFAST, foodChoices: [3, 4, 5]},
@@ -45,11 +39,6 @@ export class FoodQuizComponent implements OnInit {
    public foodItems: IFoodItem[];
    public currentFoodChoices: any = [];
 
-  // stats: any = [];
-  // currentdata = null;
-  // currentIndex = -1;
-  // title = '';
-
    constructor(private foodItemService: FoodItemService, private hobbitService: HobbitService, private route: ActivatedRoute) { }
 
    ngOnInit() {
@@ -59,10 +48,6 @@ export class FoodQuizComponent implements OnInit {
 
   populateFoodChoices() {
       this.currentFoodChoices.length = 0;
-      // for (let index of this.meals[this.currentMeal].foodChoices) {
-      //     this.currentFoodChoices.push(this.foodItems[index]);
-      // }
-
       for (let food of this.foodItems) {
           //var food = this.foodItems[i];
           if (food.meals.includes(this.currentMeal)) {
@@ -74,20 +59,6 @@ export class FoodQuizComponent implements OnInit {
       }
   }
 
-  // retrieveData() {
-  //   this.dataService.getAll().subscribe(
-  //       data => {
-  //         this.stats = data;
-  //         // now let's update the fields
-  //         this.foodItems = this.stats.foodItems;
-  //         this.meals = this.stats.meals;
-  //       },
-  //       error => {
-  //         console.log(error);
-  //       });
-  //   this.populateFoodChoices();
-  // }
-
   getFoodItems() {
       this.foodItemService
         .get()
@@ -96,21 +67,16 @@ export class FoodQuizComponent implements OnInit {
             this.populateFoodChoices();
       });
       console.error(this.foodItems);
-      //this.populateFoodChoices();
       console.error(this.currentFoodChoices);
   }
 
-  selectAnswer(//selectedFood: {name: string, sugarAmount: number}
-        selectedFood: any) {
+  selectAnswer(selectedFood: any) {
       this.answers.push(selectedFood);
       this.hobbitService.addToAnswers(selectedFood);
       this.totalSugar = this.totalSugar + selectedFood.sugarAmount;
   }
 
   answerIsCorrect(selectedSugarContent: number): boolean{
-    // let index: number = this.meals[this.currentMeal].foodChoices[0];
-    // let minSugarContent: number = Math.min(this.foodItems[index].sugarAmount,
-    // this.foodItems[index + 1].sugarAmount, this.foodItems[index + 2].sugarAmount);
     let minSugarContent: number = Math.min(this.currentFoodChoices[0].sugarAmount,
         this.currentFoodChoices[1].sugarAmount,
         this.currentFoodChoices[2].sugarAmount);
--- SugarRushSite/src/app/quiz-progress/quiz-progress.component.ts
+++ SugarRushSite/src/app/quiz-progress/quiz-progress.component.ts
@@ -9,12 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class QuizProgressComponent implements OnInit {
    @Input() hobbit?: IHobbit;
-   dailySugar: number = 100;
-   teaspoons: any;
+   dailySugar: number = 50;
+   teaspoons: any[];
 
    constructor(private hobbitService: HobbitService, private route: ActivatedRoute) { }
 
    ngOnInit(): void {
+      // this.dailySugar = this.hobbit.sugarTolerance;
+      console.log(this.dailySugar + " is the sugar cap.");
       this.convertGramsToTsp();
    }
 
@@ -22,6 +24,7 @@ export class QuizProgressComponent implements OnInit {
       let returnVal = Math.round(this.dailySugar / 4.2);
       console.log(returnVal);
       this.teaspoons = Array(returnVal);
-      console.log(this.teaspoons);
+      console.log(this.teaspoons + " is the number of teaspoons" +
+         " the hobbit can have.");
    }
 }
